373f670a719fdf5e60ab11a543afa863c58d36cc
diegog20-coder <diegog20@gmail.com>
2026-02-23 23:47:50 -0500

    Se da solución al Ejercicio 04

---
+++ 04_Ejercicio.sql
@@ -0,0 +1,5 @@
+--Ejercicio 4 ¿Cuántas transacciones de venta hemos registrado?
+--Pista: COUNT() sobre la tabla ventas.
+SELECT COUNT(*) AS Total_Ventas
+FROM ventas;
+-- la respuesta es: 1250 transacciones de venta registradas.
