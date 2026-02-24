3ebff1bccca9aa49c66efca5f6ff427228c27b8b
diegog20-coder <diegog20@gmail.com>
2026-02-23 23:41:38 -0500

    Se da solución al ejercicio 3 utilizando la pista MAX()

---
+++ 03_Ejercicio.sql
@@ -0,0 +1,5 @@
+--¿Cuál es el paracaídas más caro que tenemos?
+-- Pista: MAX() + WHERE para filtrar categoría.
+SELECT nombre, precio
+FROM productos
+WHERE precio = (SELECT MAX(precio) FROM productos WHERE categoria = 'Paracaídas');
